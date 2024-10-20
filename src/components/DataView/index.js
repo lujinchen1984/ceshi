0499356559c6d0243f604b780007841222929b42
lujinchen <274963042@qq.com>
2024-10-20 19:40:22 +0800

    增加控制模型显示功能

--- src/components/DataView/index.js
+++ src/components/DataView/index.js
@@ -13,11 +13,14 @@ import {
     CSS2DObject,
 } from "three/examples/jsm/renderers/CSS2DRenderer.js";
 import { color } from 'three/examples/jsm/nodes/Nodes.js';
+import { func } from 'three/examples/jsm/nodes/code/FunctionNode.js';
 
 export default function DataView() {
     const scene=new THREE.Scene()
     const [width,setWidth]=useState(window.innerWidth)
     const [height,setHeight]=useState(window.innerHeight-120)
+    
+    
     //相机参数
     const width_canvas=width
     const height_canvas=height  
@@ -26,6 +29,7 @@ export default function DataView() {
     useEffect(()=>{
         
         initSence(scene)
+        initModel()
         window.addEventListener('dblclick', mouseClick, false)
         return()=>{
             
@@ -37,11 +41,14 @@ export default function DataView() {
     function initSence(scene){   
         
         const renderer=new THREE.WebGLRenderer({
-            canvas:document.getElementById('containercanvas')
+            canvas:document.getElementById('containercanvas'),
+            alpha: true
         })
         renderer.setSize(width_canvas , height_canvas)
-        renderer.setClearColor('#000',.5)     
+        renderer.setClearColor('#88B9DD',.2)     
         const controls = new ArcballControls( camera, renderer.domElement, scene );
+        controls.enableGrid=false
+        controls.rotateSpeed=3
         controls.addEventListener( 'change', function () {
             renderer.render( scene, camera );
         } );
@@ -80,11 +87,13 @@ export default function DataView() {
             },
            
         }
-        
+  
         gui.add(eventObj,"Camera_X").name("X")
         gui.add(eventObj,"Camera_Y").name("Y")
         gui.add(eventObj,"Camera_Z").name("Z")
         gui.add(eventObj,"Camera_XYZ").name("XYZ")
+        
+
         var guiDom = gui.domElement;
         
         // 设置CSS样式来调整位置
@@ -95,6 +104,7 @@ export default function DataView() {
         const animate=function(){
             requestAnimationFrame(animate)    
             renderer.render( scene, camera );
+            
             
         }
 
@@ -123,8 +133,8 @@ export default function DataView() {
         gridHelper.material.opacity=0.2
         gridHelper.material.transparent=false
         gridHelper.rotateX(THREE.MathUtils.degToRad(90))
-        scene.background=new THREE.Color("#ccc")
-        scene.environment=new THREE.Color("#ccc")
+        //scene.background=new THREE.Color("#88B9DD")
+        //scene.environment=new THREE.Color("#88B9DD")
         scene.add(gridHelper)
         //平行光
         const directionLight=new THREE.DirectionalLight(0xFFFFFF,2)
@@ -140,44 +150,37 @@ export default function DataView() {
         //创建均匀照明的光源
         const light = new THREE.HemisphereLight(0xffffff, 0x444444);
         light.position.set(0, 0, 10000);
-        scene.add(light);      
-        initModel() 
+        scene.add(light);  
+            
+        
         animate()
 
     }
     function initModel(){
+
         var planegeometry = new THREE.PlaneGeometry(1000, 1000, 1);    
-        var planematerial = new THREE.MeshBasicMaterial({ color: 0x00ff00,side: THREE.DoubleSide });
+        var planematerial = new THREE.MeshBasicMaterial({ color: 0xaeb2ae,side: THREE.DoubleSide,transparent: true,opacity: 0,});
         var plane = new THREE.Mesh(planegeometry, planematerial);
-        plane.position.set(0, 0, 1.23);
+        plane.position.set(0, 0, 1);
         scene.add(plane);
-        var planegeometry1 = new THREE.PlaneGeometry(1000, 1000, 1);    
-        var planematerial1 = new THREE.MeshBasicMaterial({ color: 0xff0000,side: THREE.DoubleSide });
-        var plane1 = new THREE.Mesh(planegeometry1, planematerial1);
-        // 设置平面位置
-        plane1.position.set(0, 0, 5);
-        scene.add(plane1);
-        //加载数模
-        const objLoader=new OBJLoader()
-        objLoader.load(
-            '/static/mymodel/MH AMG_stp.obj',
-            function(object){             
-                //object.rotateX(-Math.PI/2)                
-                //scene.add(object)
-                // 设置材质为线框模式
-                object.traverse((child) => {
-                    if (child.isMesh) {
-                    child.material.wireframe = true; // 显示线框
-                    }
-                });
-            },
-            function(xhr){
-                //console.log((xhr.loaded/xhr.total*100)+'% loaded')
-            },
-            function(error){
-                console.log('an error happend'+error)
-            }
-        )
+    
+        plane.name='data'
+        const gui2 = new GUI();
+        //创建GUI
+        gui2.addColor(planematerial, 'color').name('颜色').onChange((color) => {
+            planematerial.color.set(color);
+        });
+        gui2.add(planematerial, 'opacity').min(0).max(1).step(0.01).name('透明度').onChange((value) => {
+            planematerial.opacity = value;          
+  
+          });
+        var gui2Dom = gui2.domElement;
+    
+        // 设置CSS样式来调整位置
+        gui2Dom.style.position = 'absolute';
+        gui2Dom.style.left = '20px'; // 根据需要调整到所需的水平位置
+        gui2Dom.style.top = '220px'; // 根据需要调整到所需的垂直位置
+  
         
     }
     function mouseClick(event) {  
@@ -193,17 +196,22 @@ export default function DataView() {
         if (intersects.length > 0) {
             // 取第一个交点
             const intersection = intersects[0];    
-            const position = intersection.point;
-            // 获取交点的法向量
-            const normal = intersection.face.normal;
-            var color = 0x0000ff; // 箭头颜色
-            // 创建ArrowHelper对象
-            var arrow = new THREE.ArrowHelper(normal, position, 100, color); // 1表示箭头的长度为1单位
-            scene.add(arrow);
+            console.log(intersects[0])
+            if (intersects[0].object.name=='data'){
+                const position = intersection.point;
+                // 获取交点的法向量
+                const normal = intersection.face.normal;
+                var color = 0x0000ff; // 箭头颜色
+                // 创建ArrowHelper对象
+                var arrow = new THREE.ArrowHelper(normal, position, 100, color); // 1表示箭头的长度为1单位
+                scene.add(arrow);
+            }
+           
            
         }
     }
     // 监听鼠标移动事件
+
     
   return (
     <>
